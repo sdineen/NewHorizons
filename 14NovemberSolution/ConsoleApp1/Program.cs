6e41d49ee533ee862044cbbaa285092ffbe9b0f1
sdineen <dineen701@gmail.com>
2022-11-14 14:41:58 +0000

    methods

---
+++ 14NovemberSolution/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+
+            double result = Factorial(5);
+            Console.WriteLine(result);
+            int result2 = ArithmeticSeries(10);
+            Console.WriteLine(result2);
+            double result3 = GeometricSeries(10);
+        }
+
+        //sum of 2^0 + 2^1 .... + 2^10
+        private static double GeometricSeries(int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        //sum of 0 + 1 + 2 ... + 10
+        private static int ArithmeticSeries(int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        //5 x 4 x 3 x 3
+        private static double Factorial(int n)
+        {
+            return 0;
+        }
+    }
+}
