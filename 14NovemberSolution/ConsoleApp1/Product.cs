43f8c699a0e4260f037c384f6fc00cf38bda117a
sdineen <dineen701@gmail.com>
2022-11-14 15:59:19 +0000

    Product

---
+++ 14NovemberSolution/ConsoleApp1/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Product
+    {
+        private int id;
+        private string name;
+        private double costPrice;
+
+        public int Id 
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Name
+        { 
+            get { return name; } 
+            set { name = value; }
+        }
+
+        public double CostPrice
+        {
+            get { return costPrice; }
+            set { costPrice = value; }
+        }
+    }
+}
