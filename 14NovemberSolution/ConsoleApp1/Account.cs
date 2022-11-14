12184cfcdeaa54fb62d9b850853b0e9361d71914
sdineen <dineen701@gmail.com>
2022-11-14 15:42:50 +0000

    Account

---
+++ 14NovemberSolution/ConsoleApp1/Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Account
+    {
+        private string id;
+        private string name;
+        private string password;
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+
+
+    }
+}
--- 14NovemberSolution/ConsoleApp1/Program.cs
+++ 14NovemberSolution/ConsoleApp1/Program.cs
@@ -1,53 +1,21 @@
 ﻿
+using ConsoleApp1;
+
 namespace MyApp
 {
     internal class Program
     {
         static void Main(String[] args)
         {
-            int result1 = ArithmeticSeries(10);
-            Console.WriteLine(result1);
-            double result2 = GeometricSeries(10);
-            Console.WriteLine(result2);
-            double result3 = Factorial(5);
-            Console.WriteLine(result3);
-            double result4 = FactorialRecursion(5);
-            Console.WriteLine(result4);
+            //acc1 is an object reference variable
+            Account acc1 = new Account(); //builds an Account object
+            acc1.Name = "Bill"; //sets the name variable of the object
+            acc1.Id = "account1";
+            acc1.Password = "password";
+
+            Console.WriteLine($"id: {acc1.Id} name: {acc1.Name}");
         }
 
-        private static double FactorialRecursion(int n)
-        {
-            return n==1 ? 1 : n * FactorialRecursion(n-1);
-        }
-
-        private static double Factorial(int n)
-        {
-            double result = 1;
-            for (; n>1; n--)
-            {
-                result *= n;
-            }
-            return result;
-        }
-
-        private static double GeometricSeries(int n)
-        {
-            double result = 0;
-            for (int i = 0; i <=n; i++)
-            {
-                result += Math.Pow(2, i);
-            }
-            return result;
-        }
-
-        private static int ArithmeticSeries(int n)
-        {
-            int result = 0;
-            for (int i = 0; i <=n ; i++)
-            {
-                result += i;
-            }
-            return result;
-        }
+       
     }
 }
