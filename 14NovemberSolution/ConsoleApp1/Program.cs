107410aeaa6941f9370ee5ae0a9d6259bd993ab7
sdineen <dineen701@gmail.com>
2022-11-14 14:57:17 +0000

    methods

--- 14NovemberSolution/ConsoleApp1/Program.cs
+++ 14NovemberSolution/ConsoleApp1/Program.cs
@@ -5,30 +5,49 @@ namespace MyApp
     {
         static void Main(String[] args)
         {
-
-            double result = Factorial(5);
-            Console.WriteLine(result);
-            int result2 = ArithmeticSeries(10);
+            int result1 = ArithmeticSeries(10);
+            Console.WriteLine(result1);
+            double result2 = GeometricSeries(10);
             Console.WriteLine(result2);
-            double result3 = GeometricSeries(10);
+            double result3 = Factorial(5);
+            Console.WriteLine(result3);
+            double result4 = FactorialRecursion(5);
+            Console.WriteLine(result4);
         }
 
-        //sum of 2^0 + 2^1 .... + 2^10
-        private static double GeometricSeries(int n)
+        private static double FactorialRecursion(int n)
         {
-            throw new NotImplementedException();
+            return n==1 ? 1 : n * FactorialRecursion(n-1);
         }
 
-        //sum of 0 + 1 + 2 ... + 10
-        private static int ArithmeticSeries(int n)
-        {
-            throw new NotImplementedException();
-        }
-
-        //5 x 4 x 3 x 3
         private static double Factorial(int n)
         {
-            return 0;
+            double result = 1;
+            for (; n>1; n--)
+            {
+                result *= n;
+            }
+            return result;
+        }
+
+        private static double GeometricSeries(int n)
+        {
+            double result = 0;
+            for (int i = 0; i <=n; i++)
+            {
+                result += Math.Pow(2, i);
+            }
+            return result;
+        }
+
+        private static int ArithmeticSeries(int n)
+        {
+            int result = 0;
+            for (int i = 0; i <=n ; i++)
+            {
+                result += i;
+            }
+            return result;
         }
     }
 }
