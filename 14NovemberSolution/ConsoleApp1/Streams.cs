068b755ef731dff2649be2d3e569559c13e77725
sdineen <dineen701@gmail.com>
2022-11-15 12:52:13 +0000

    exceptions

---
+++ 14NovemberSolution/ConsoleApp1/Streams.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Streams
+    {
+        public static void WriteToFile(string text)
+        {
+            //UnauthorizedAccessException
+            File.WriteAllText(@"C:\Users\Owner\Documents\temp\file.txt", text);
+            //DirectoryNotFoundException
+            File.WriteAllText(@"Y:\file.txt", text);
+        }
+
+        static void Main(string[] args)
+        {
+            WriteToFile("some text");
+        }
+    }
+
+}
