9d2e9703798dfebd97a754fb43e2c38ee6ed276b
sdineen <dineen701@gmail.com>
2022-11-16 15:56:39 +0000

    Update CollectionExamples.cs

--- 14NovemberSolution/ConsoleApp1/CollectionExamples.cs
+++ 14NovemberSolution/ConsoleApp1/CollectionExamples.cs
@@ -1,15 +1,49 @@
 ﻿using Examples.LINQ;
+using System;
 
 
 namespace ConsoleApp1
 {
+
+
+    public delegate int Delegate1(int i);
+    public delegate TResult Delegate2<TSource, TResult>(TSource t);
+
     internal class CollectionExamples
     {
+        public static int DoubleIt(int i)
+        {
+            return i * 2;
+        }
+
+        public static bool IsEven(int i)
+        {
+            return i % 2 == 0;
+        }
+
+
         static void Main(string[] args)
         {
-            int i = 17;
-            bool result = i.IsPrime();
-            Console.WriteLine(result);
+            Delegate1 d1 = new Delegate1(DoubleIt);
+            Console.WriteLine(d1.Invoke(5));
+            //or simply
+            Console.WriteLine(d1(5));
+
+
+            Delegate2<int, bool> d2 = new Delegate2<int, bool>(IsEven);
+            Console.WriteLine(d2(7));
+
+            Delegate2<int, bool> d3 = new Delegate2<int, bool>(i => i%2 == 0);
+            Console.WriteLine(d2(7));
+
+
+            Func<int, bool> func = new Func<int, bool>(i => i % 2 == 0);
+            Console.WriteLine(func(7));
+
+
+            //int i = 17;
+            //bool result = i.IsPrime();
+            //Console.WriteLine(result);
 
 
 
@@ -39,24 +73,41 @@ namespace ConsoleApp1
 
 
 
-            //ICollection < Product > products = new List<Product> {
-            //    new Product(1, "Pedigree Chum", 0.70, 1.42),
-            //    new Product(2, "Knife", 0.60, 1.31),
-            //    new Product(3, "Fork", 0.75, 1.57),
-            //    new Product(4, "Spaghetti", 0.90, 1.92),
-            //    new Product(5, "Cheddar Cheese", 0.65, 1.47),
-            //    new Product(6, "Bean bag", 15.20, 32.20),
-            //    new Product(7, "Bookcase", 22.30, 46.32),
-            //    new Product(8, "Table", 55.20, 134.80),
-            //    new Product(9, "Chair", 43.70, 110.20),
-            //    new Product(10, "Doormat", 3.20, 7.40)
-            //};
+            ICollection<Product> products = new List<Product> {
+                new Product(1, "Pedigree Chum", 0.70, 1.42),
+                new Product(2, "Knife", 0.60, 1.31),
+                new Product(3, "Fork", 0.75, 1.57),
+                new Product(4, "Spaghetti", 0.90, 1.92),
+                new Product(5, "Cheddar Cheese", 0.65, 1.47),
+                new Product(6, "Bean bag", 15.20, 32.20),
+                new Product(7, "Bookcase", 22.30, 46.32),
+                new Product(8, "Table", 55.20, 134.80),
+                new Product(9, "Chair", 43.70, 110.20),
+                new Product(10, "Doormat", 3.20, 7.40)
+            };
+
+            double average = products
+                .Where(p=>p.Name.StartsWith("B"))
+                .Average(p => p.CostPrice);
+            Console.WriteLine(average);
+
+            var filteredProducts =
+                products
+                .Where(p => p.Name.StartsWith("C"))
+                .Select(p => new { p.Id, p.Name });
+
+
+
+            foreach (var product in filteredProducts)
+            {
+                Console.WriteLine(product.Name);
+            }
 
             //IEnumerable<Product> filteredProducts =
             //    from p in products
             //    where p.CostPrice > 10
             //    select p;
-            
+
             //foreach (Product product in filteredProducts)
             //{
             //    Console.WriteLine(product.Name);
