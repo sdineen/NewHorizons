e9cb568c2fbbb47d7d4420fa8f93156c571459f2
sdineen <dineen701@gmail.com>
2022-11-17 10:00:07 +0000

    db

---
+++ 14NovemberSolution/ConsoleApp1/Database.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Database
+    {
+        static void Main(string[] args)
+        {
+            SqliteConnection connection = new SqliteConnection("Data Source=database.db");
+            connection.Open();
+            SqliteCommand cmd = new SqliteCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "create table if not exists accounts(id integer primary key, name text not null, password text not null); ";
+            bool tableCreated = cmd.ExecuteNonQuery() == 1;
+            cmd.CommandText = "insert into accounts (name, password)  values('John Smith', 'test'); ";
+            int rowsInserted = cmd.ExecuteNonQuery();
+            connection.Close();
+
+        }
+    }
+}
