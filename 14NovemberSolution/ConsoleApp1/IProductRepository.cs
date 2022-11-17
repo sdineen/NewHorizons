c9659d52e2b2a8521bd698dc5c8ec7dfceb38b35
sdineen <dineen701@gmail.com>
2022-11-17 10:49:12 +0000

    Create IProductRepository.cs

---
+++ 14NovemberSolution/ConsoleApp1/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public interface IProductRepository
+    {
+        ICollection<Product>? SelectAll();
+        Product? SelectById(int id);
+        bool Create(Product product);
+        bool Delete(int id);
+        bool Update(Product product);
+
+    }
+}
