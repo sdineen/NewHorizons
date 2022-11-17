210011961e51325f5753a76c259e8f1869050aed
sdineen <dineen701@gmail.com>
2022-11-17 15:02:44 +0000

    accountRepository

--- 14NovemberSolution/ConsoleApp1/ECommerceContext.cs
+++ 14NovemberSolution/ConsoleApp1/ECommerceContext.cs
@@ -14,7 +14,28 @@ namespace ConsoleApp1
         {
             optionsBuilder.UseSqlite("Data Source=database.db");
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product(1, "Pedigree Chum", 0.70, 1.42),
+                new Product(2, "Knife", 0.60, 1.31),
+                new Product(3, "Fork", 0.75, 1.57),
+                new Product(4, "Spaghetti", 0.90, 1.92),
+                new Product(5, "Cheddar Cheese", 0.65, 1.47),
+                new Product(6, "Bean bag", 15.20, 32.20),
+                new Product(7, "Bookcase", 22.30, 46.32),
+                new Product(8, "Table", 55.20, 134.80),
+                new Product(9, "Chair", 43.70, 110.20),
+                new Product(10, "Doormat", 3.20, 7.40)
+                );
 
+            modelBuilder.Entity<Account>().HasData(
+             new Account("acc1", "John Smith",
+                       "9F86D081884C7D659A2FEAA0C55AD015A3BF4F1B2B0B822CD15D6C15B0F00A08"),
+            new Account("acc2", "Jane Jones",
+                       "9F86D081884C7D659A2FEAA0C55AD015A3BF4F1B2B0B822CD15D6C15B0F00A08")
+           );
+        }
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Account> Accounts { get; set; }
--- 14NovemberSolution/ConsoleApp1/EfAccountRepository.cs
+++ 14NovemberSolution/ConsoleApp1/EfAccountRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,46 @@ namespace ConsoleApp1
 {
     public class EfAccountRepository : IAccountRepository
     {
+        private ECommerceContext context;
+        public EfAccountRepository(ECommerceContext context)
+        {
+            this.context = context;
+        }
+        public bool Create(Account account)
+        {
+            context.Accounts.Add(account);
+            int updates = context.SaveChanges();
+            return updates > 0;
+        }
+
+        public bool Delete(string accountId)
+        {
+            Account account = context.Accounts.Find(accountId);
+            if (account == null)
+            {
+                return false;
+            }
+            context.Remove(account);
+            return context.SaveChanges() == 1;
+
+        }
+
+        public Account SelectById(string id)
+        {
+            return context.Accounts.Find(id);
+        }
+
+        public bool Update(Account modifiedAccount)
+        {
+            Account? account = context.Accounts.Find(modifiedAccount.Id);
+            if (account == null)
+            {
+                return false;
+            }
+            account.Name = modifiedAccount.Name;
+            account.Password = modifiedAccount.Password;
+            bool added = context.SaveChanges() == 1;
+            return added;
+        }
     }
 }
